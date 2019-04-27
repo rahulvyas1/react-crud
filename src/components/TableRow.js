4e170c65ed536c7b6b30d0a2decb71ebad261da4
Rahul Vyas <rahulvyas@plaak.com>
2019-04-28 02:03:17 +0530

    working application

---
+++ src/components/TableRow.js
@@ -0,0 +1,33 @@
+// TableRow.js
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.person_name}
+          </td>
+          <td>
+            {this.props.obj.business_name}
+          </td>
+          <td>
+            {this.props.obj.business_gst_number}
+          </td>
+          <td>
+            <button className="btn btn-primary">Edit</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+          <td>
+          <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
